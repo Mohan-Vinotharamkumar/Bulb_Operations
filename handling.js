2bfa7fa9137880a2fccaecc9a27303bc1057697a
Mohan-Vinotharamkumar <navaneethamohan2002@gmail.com>
2023-05-25 14:07:17 +0530

    25/05/2023

---
+++ handling.js
@@ -0,0 +1,9 @@
+function switch_on()
+{
+    document.getElementById("bulb").style.backgroundColor='#FEFF86';
+}
+
+function switch_off()
+{
+    document.getElementById("bulb").style.backgroundColor="#BACDDB"
+}
